4ea23e6214399502b366943d473f20d267353674
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-28 21:40:07 +1000

    made api folder and added youtube key

---
+++ src/apis/youtube.js
@@ -0,0 +1,1 @@
+const KEY = 'AIzaSyAvzMOE3FB9w2AyzcVqjiWz8k63YWWZxmY';
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,20 +3,25 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = {term: ''};
 
-  onInputChange = e => {
+  onInputChange = (e) => {
+    this.setState({term: e.target.value});
+  };
 
-  }
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    // Make sure we call callback from parent component
+  };
 
   render() {
     return (
       <div className="ui segment search-bar">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Video Search</label>
-            <input 
-            type="text" 
-            value={this.state.term} 
-            onChange={this.onInputChange} 
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
             />
           </div>
         </form>
