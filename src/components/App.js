b90e24a593f75d18fb04209cfe5928f663547cf7
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-29 20:22:12 +1000

    added youtube style styling

--- src/components/App.js
+++ src/components/App.js
@@ -23,17 +23,27 @@ class App extends React.Component {
     this.setState({videos: response.data.items});
   };
 
-onVideoSelect = (video) => {
+  onVideoSelect = (video) => {
     this.setState({selectedVideo: video});
-}
-
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList onVideoSelect = {this.onVideoSelect} videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
