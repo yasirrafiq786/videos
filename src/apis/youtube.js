fc5136c518dbb250a5f7195e6cf90a7864b99981
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-29 17:35:54 +1000

    api working

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAvzMOE3FB9w2AyzcVqjiWz8k63YWWZxmY';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    part: 'snippet',
-    type: 'video',
-    maxResults: '5',
-    key: KEY,
-  },
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+const KEY = 'AIzaSyAvzMOE3FB9w2AyzcVqjiWz8k63YWWZxmY';
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-      console.log(term);
+  state = {videos: []};
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        type: 'video',
+        maxResults: '5',
+        key: KEY,
+      },
+    });
+
+    this.setState({videos: response.data.items});
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        
       </div>
     );
   }
