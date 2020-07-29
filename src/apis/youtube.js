dfe7871266c96802fcf71b699c30ffed28b7b5cb
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-29 16:52:01 +1000

    added youtube api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAvzMOE3FB9w2AyzcVqjiWz8k63YWWZxmY';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: '5',
+    key: KEY,
+  },
+});
